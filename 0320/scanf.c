a9e2e7f8a53dfaaa00ad090550466f82fbd3ca04
Seongjun Lee <andrew4235@naver.com>
2024-03-27 19:36:15 +0900

    first commit

---
+++ 0320/scanf.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+/*
+int main(void)
+{
+	int a;
+	printf("숫자 하나를 입력하세오 : ");
+	scanf("%d", &a);
+
+	printf("입력한 숫자는 %d입니다.\n", a);
+
+    %: 나머지
+	/ : 나누기 연산자
+
+	수동 형변환
+	int a,b;
+	(double)a/(double)b;
+
+	'증감 연산자'
+
+	a=5
+
+	a = a+1; = : 대입 연산자. 왼쪽 <- 오른쪽
+	a += 1;
+
+	a++; 
+	++a;
+
+	'관계 연산자': 참이면 true(1), 거짓이면 false(0)를 리턴하는 연산자
+
+	a>b a>=b a<b a==b a!=b (!는 not의 의미!)
+
+	'논리연산자' : <- and or not
+
+	a&&b, !a, a||b (or의 의미)
+
+
+
+	return 0;
+}
+*/
